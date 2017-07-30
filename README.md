8a5f20c949cafa63d7667b536743109682b19b80
Matteo Capitanio <matteo.capitanio@gmail.com>
2017-07-30 14:08:59 -0700

    First implementation of SCHEMA_REGISTRY CSD

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# cloudera-csd-gen
-Cloudera CSDs generator
+## **Parrot Cloudera CSDs!**
+
+
+
+This project contains the Cloudera **Custom Service Descriptors** (CSD) for all the Parrot Distribution components.
+
+### **Build**
+
+#### **Prerequisites**
+
+- Maven 3.2.x (or greater)
+
+#### **Command**
+
+    ./parrot-csd build
+   
+### **Installation**
+
+#### **Command**
+
+To install in the Cloudera default directory `/opt/cloudera/csd`:
+
+    ./parrot-csd install
+
+To install in a specific directory which has to be configured in the Cloudera Admin console:
+
+    ./parrot-csd install -d=INSTALL_DIR
+
+Restart the Cloudera Manager Server:
+    
+        sudo service cloudera-scm-server restart
+
+Finally restart the Cloudera Management Service in the Cloudera Admin Console
+
+You can find more detailed instruction on the [Cloudera Add-on Services](https://www.cloudera.com/documentation/enterprise/5-11-x/topics/cm_mc_addon_services.html) documentation.
+
+To be able to configure and use the Added service you must now **download**, **distribute** and **activate** the Parrot parcels.
+To do that you can go to the Parcels section in the Cloudera Admin Console ( `Hosts -> Parcels` ).
+
+Some of the Parrot services depends on other **CDH** services. Following is the dependency matrix:
+
+| Parrot service    | Depends on      | Version   |
+|-------------------|-----------------|-----------|
+| SCHEMA_REGISTRY   | ZOOKEEPER       ||
+| SCHEMA_REGISTRY_UI| SCHEMA_REGISTRY ||
+| KAFKA_REST        | KAFKA           |`>= 2.2.0`  |
+| KAFKA_TOPICS_UI   | KAFKA           |`>= 2.2.0` |
+| PARROT            | KAFKA           |`>= 2.2.0` |
+| KAFKA_CONNECT_UI  | PARROT          ||
+
--- SCHEMA_REGISTRY/src-gen/scripts/control.sh
+++ SCHEMA_REGISTRY/src-gen/scripts/control.sh
@@ -5,30 +5,49 @@ set -x
 DEFAULT_SCHEMA_REGISTRY_HOME=/usr/share/java/schema-registry
 SCHEMA_REGISTRY_HOME=${SCHEMA_REGISTRY_HOME:-$DEFAULT_SCHEMA_REGISTRY_HOME}
 
-# example first line of version file: version=0.1.0-3.2.2
-SCHEMA_REGISTRY_VERSION=$(grep "^version=" $SCHEMA_REGISTRY_HOME/../../cloudera/cdh_version.properties | cut -d '=' -f 2)
+# Example first line of version file: version=0.1.0-3.2.2
+SCHEMA_REGISTRY_VERSION=$(grep "^version=" $SCHEMA_REGISTRY_HOME/cloudera/cdh_version.properties | cut -d '=' -f 2)
 echo "Confluent Schema Registry version found: ${SCHEMA_REGISTRY_VERSION}"
-
-# For better debugging
-echo -e "Date: `date`"
-echo -e "Host: $HOST"
-echo -e "Pwd: `pwd`"
-echo -e "CONF_DIR: $CONF_DIR"
-echo -e "SCHEMA_REGISTRY_HOME: $KAFKA_HOME"
-echo -e "SCHEMA_REGISTRY_VERSION: $SCHEMA_REGISTRY_VERSION"
-echo -e "Zookeeper Quorum: $ZK_QUORUM"
-echo -e "Zookeeper Chroot: $CHROOT"
-echo -e "PORT: $PORT"
 
 # Generating Zookeeper quorum
 QUORUM=$ZK_QUORUM
 if [[ -n $CHROOT ]]; then
-	QUORUM="${QUORUM}${CHROOT}"
+  QUORUM="${QUORUM}${CHROOT}"
 fi
-echo "Final Zookeeper Quorum is $QUORUM"
 
-# Replace kafkastore.connection.urlplaceholder
+# Add Listener
+if [[ ${SSL_ENABLED} == "true" ]]; then
+    LISTENERS="https://${HOST}:${SSL_PORT}"
+else
+    LISTENERS="http://${HOST}:${PORT}"
+fi
+
+echo -e "######################################################################################"
+echo -e "# PARROT DISTRIBUTION - SCHEMA REGISTRY"
+echo -e "#-------------------------------------------------------------------------------------"
+echo -e "# DATE:                    `date`"
+echo -e "# PORT:                    $PORT"
+echo -e "# HOST:                    $HOST"
+echo -e "# PWD:                     `pwd`"
+echo -e "# CONF_DIR:                $CONF_DIR"
+echo -e "# SCHEMA_REGISTRY_HOME:    $SCHEMA_REGISTRY_HOME"
+echo -e "# SCHEMA_REGISTRY_VERSION: $SCHEMA_REGISTRY_VERSION"
+echo -e "# ZK_QUORUM:               $QUORUM"
+echo -e "#-------------------------------------------------------------------------------------"
+echo -e "# SSL_ENABLED:             $SSL_ENABLED"
+echo -e "# SSL_PORT:                $SSL_PORT"
+echo -e "# SSL_KEY_PASSWORD:        $SSL_KEY_PASSWORD"
+echo -e "# SSL_KEYSTORE_LOCATION:   $SSL_KEYSTORE_LOCATION"
+echo -e "# SSL_KEYSTORE_PASSWORD:   $SSL_KEYSTORE_PASSWORD"
+echo -e "# SSL_TRUSTSTORE_LOCATION: $SSL_TRUSTSTORE_LOCATION"
+echo -e "# SSL_TRUSTSTORE_PASSWORD: $SSL_TRUSTSTORE_PASSWORD"
+echo -e "######################################################################################"
+
+# Replace kafkastore.connection.url placeholder with ZooKeeper Quorum
 perl -pi -e "s#\#kafkastore.connection.url={{QUORUM}}#kafkastore.connection.url=${QUORUM}#" $CONF_DIR/schema-registry.properties
 
-# And finally run Kafka itself
-exec $SCHEMA_REGISTRY_HOME/bin/schema-registry-start.sh $CONF_DIR/schema-registry.properties
+# Replace listeners placeholder with LISTENERS
+perl -pi -e "s#\#listeners={{LISTENERS}}#listeners=${LISTENERS}#" $CONF_DIR/schema-registry.properties
+
+# Run Confluent Schema Registry
+exec $SCHEMA_REGISTRY_HOME/bin/schema-registry-start $CONF_DIR/schema-registry.properties
